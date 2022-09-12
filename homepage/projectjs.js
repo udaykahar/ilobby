312c5556f9144bc00240378dbcccc7d7f9d8a843
udaykahar <udaykahar29@gmail.com>
2022-09-12 22:00:16 +0530

    added vivin's homepages

---
+++ homepage/projectjs.js
@@ -0,0 +1,37 @@
+// const options={
+//     bottom: '32px', // default: '32px'
+//     right: '32px', // default: '32px'
+//     left: 'unset', // default: 'unset'
+//     time: '0.5s', // default: '0.3s'
+//     mixColor: '#111', // default: '#fff'
+//     backgroundColor: '#111',  // default: '#fff'
+//     buttonColorDark: '#fff',  // default: '#100f2c'
+//     buttonColorLight: '#ffffff', // default: '#fff'
+//     saveInCookies: false, // default: true,
+//     label: '🌓', // default: ''
+//     autoMatchOsTheme: true // default: true
+// }
+// const darkmode = new Darkmode(options);
+// darkmode.showWidget();
+
+
+
+// function logochange()
+// {
+//     document.getElementById("mainlogo").src="https://www.designhill.com/tools/logo-maker";
+// }
+
+// const options={
+//     saveInCookies: true, // default: true,
+// }
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "/homepage/img/sun.png";
+    }
+    else{
+        icon.src = "/homepage/img/moon.png";
+    }
+}
